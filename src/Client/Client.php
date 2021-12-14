d954e45b4e88d34d7d8a5bd1b2c4e9c9fee3970e
Zsolt148 <b.zsolt148@gmail.com>
2021-12-14 15:55:05 +0100

    xmlreading exception fix

--- src/Client/Client.php
+++ src/Client/Client.php
@@ -717,7 +717,7 @@ class Client
                 {
                     $this->writeCredentials($writer);
                     $writer->writeElement('eszamla', $this->stringifyBoolean($invoice->isElectronic));
-                    $writer->writeElement('kulcstartojelszo', '');
+                    //$writer->writeElement('kulcstartojelszo', '');
                     $writer->writeElement('szamlaLetoltes', $this->stringifyBoolean(!$withoutPdf));
                     $writer->writeElement('valaszVerzio', 2);
                     $writer->writeElement('aggregator', '');
@@ -960,7 +960,7 @@ class Client
                 {
                     $this->writeCredentials($writer);
                     $writer->writeElement('eszamla', $this->stringifyBoolean($invoice->isElectronic));
-                    $writer->writeElement('kulcstartojelszo', '');
+                    //$writer->writeElement('kulcstartojelszo', '');
                     $writer->writeElement('szamlaLetoltes', $this->stringifyBoolean(false));
                     $writer->writeElement('szamlaLetoltesPld', 1);
                 }
@@ -1020,7 +1020,7 @@ class Client
                     {
                         $this->writeCredentials($writer);
                         $writer->writeElement('eszamla', $this->stringifyBoolean($invoice->isElectronic));
-                        $writer->writeElement('kulcstartojelszo', '');
+                        //$writer->writeElement('kulcstartojelszo', '');
                         $writer->writeElement('szamlaLetoltes', $this->stringifyBoolean(true));
                         $writer->writeElement('szamlaLetoltesPld', 1);
                     }
